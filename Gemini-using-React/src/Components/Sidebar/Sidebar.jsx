5ba3d8c31e0f1355e005a4061356e24020de048b
Pranjal Rai <pranjalrai2909@gmail.com>
2025-03-24 01:02:58 +0530

    Gemini Completed

--- Gemini-using-React/src/Components/Sidebar/Sidebar.jsx
+++ Gemini-using-React/src/Components/Sidebar/Sidebar.jsx
@@ -1,44 +1,61 @@
-import React, {useState} from 'react'
-import './Sidebar.css'
-import {assets} from '../../assets/assets.js'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets.js";
+import { useContext } from "react";
+import { Context } from "../../context/context.jsx";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+  const { onSent, prevPrompts, setRecentPrompt,newChat} = useContext(Context);
 
-  const [extended, setExtended] = useState(false)
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
 
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="top">
-        <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
-        <div className="new-chat">
-            <img src={assets.plus_icon} alt="" />
-            {extended?<p>New Chat</p>:null}
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div onClick={() => newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
         </div>
-        {extended?
-        <div className="recent">
+        {extended ? (
+          <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>What is React....</p>    
-            </div>    
-        </div>:null}  
+            {prevPrompts.map((item, index) => {
+              return (
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="message icon" />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
-            <img src={assets.question_icon} alt="" />
-            {extended?<p>Help</p>:null}
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
         </div>
         <div className="bottom-item recent-entry">
-            <img src={assets.history_icon} alt="" />
-            {extended?<p>Activity</p>:null}
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
         </div>
         <div className="bottom-item recent-entry">
-            <img src={assets.setting_icon} alt="" />
-            {extended?<p>Settings</p>:null}
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
